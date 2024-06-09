9933f6894db58084d5e3e101f04e14bab9a142b6
Alex Costa <89145796+AlexBCosta@users.noreply.github.com>
2024-06-08 23:34:31 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,15 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
-		Seller seller = new Seller(10, "Alex", "alex.b.costa28@gmail.com", new Date(), 2500.00, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
